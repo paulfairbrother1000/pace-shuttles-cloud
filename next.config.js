966742782be3432e674cd0d322ec7d08e21cd3bc
paulfairbrother1000 <paul.fairbrother@beyondservicemanagement.com>
2025-10-03 23:24:13 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 // next.config.js
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '**.supabase.co', pathname: '/storage/v1/object/**' },
+      {
+        protocol: 'https',
+        hostname: 'bopvaaexicvdueidyvjd.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
     ],
   },
 };
+
+module.exports = nextConfig;
