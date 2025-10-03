381e96eb4f413357ed13152ec4062c889f8d09c5
paulfairbrother1000 <paul.fairbrother@beyondservicemanagement.com>
2025-10-03 13:29:28 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,15 +2,8 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "bopvaaexicvdueidyvjd.supabase.co",
-        pathname: "/storage/v1/object/public/**",
-      },
-      // add any other hosts you use for images:
-      // { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
+      { protocol: 'https', hostname: '**.supabase.co', pathname: '/storage/v1/object/**' },
     ],
-    deviceSizes: [360, 414, 640, 768, 1024, 1280, 1536],
   },
 };
 
