daf79824615e6dd4bc37a2c7822ef2fa35a43390
paulfairbrother1000 <paul.fairbrother@beyondservicemanagement.com>
2025-10-23 13:14:52 +0100

    8

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+export const dynamic = "force-dynamic"; // (optional, but avoids SSG on this page)
+
 
 import { useEffect, useMemo, useState } from "react";
 import { createBrowserClient } from "@supabase/ssr";
